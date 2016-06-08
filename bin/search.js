2372ee23c8d0e3a3ad42ece621fe62a7bf6bf5b7
Raymond Pulver IV <raymond.pulver_iv@uconn.edu>
2016-06-08 16:11:01 -0400

    added fully testable web scraping

--- bin/search.js
+++ bin/search.js
@@ -1,120 +1,129 @@
 #!/usr/bin/env node
 
-var basename = require('../lib/basename');
-var sprintf = require('sprintf');
+"use strict";
 
-var processName;
+const path = require('path'),
+    sprintf = require('sprintf'),
+    log = console.log,
+    exit = process.exit;
 
-(function beforeRequire () {
-  var log = console.log,
-      exit = process.exit;
-  process.title = basename(process.argv[1]);
-  if (process.argv.length < 3) {
-    help();
-    exit(0);
-  }
-  for (var i = 0; i < process.argv.length; i++) {
-    if (process.argv[i] === '-v' || process.argv[i] === '--version') {
-      log(require('../package').version);
-      exit(0);
+(function beforeRequire() {
+    process.title = path.basename(process.argv[1]);
+    if (process.argv.length < 3) {
+        help();
+        exit(1);
     }
-    if (process.argv[i] === '-h' || process.argv[i] === '--help') {
-      help();
-      exit(0);
+    for (let i = 0; i < process.argv.length; i++) {
+        if (process.argv[i] === '-v' || process.argv[i] === '--version') {
+            log(require('../package').version);
+            exit(0);
+        }
+        if (process.argv[i] === '-h' || process.argv[i] === '--help') {
+            help();
+            exit(0);
+        }
     }
-  }
-  function help () {
-    log('Usage: ' + process.title + ' [-mvh] [-c CATEGORY] [-p PAGE] SEARCH');
-    var fmt = '       %-15s %-25s %-30s';
-    log(sprintf(fmt, '-v,', '--version', 'display version number'));
-    log(sprintf(fmt, '-h,', '--help', 'display this help'));
-    log(sprintf(fmt, '-m', '--magnet', 'display magnet links instead of torrent URLs'));
-    log(sprintf(fmt, '-n', '--no-color', 'disable colored output'));
-//    log(sprintf(fmt, '-n', '--no-color', 'disable color explicitly'));
-    log(sprintf(fmt, '-c [category],', '--category=[category]', 'specify category of torrent'));
-    log(sprintf(fmt, '-p [page #],', '--page=[page #]', 'specify result page number'));
-  }
+
+    function help() {
+        log('Usage: ' + process.title + ' [-mvh] [-c CATEGORY] [-p PAGE] SEARCH');
+        let fmt = '       %-15s %-25s %-30s';
+        log(sprintf(fmt, '-v,', '--version', 'display version number'));
+        log(sprintf(fmt, '-h,', '--help', 'display this help'));
+        log(sprintf(fmt, '-m', '--magnet', 'display magnet links instead of torrent URLs'));
+        log(sprintf(fmt, '-n', '--no-color', 'disable colored output'));
+        //    log(sprintf(fmt, '-n', '--no-color', 'disable color explicitly'));
+        log(sprintf(fmt, '-c [category],', '--category=[category]', 'specify category of torrent'));
+        log(sprintf(fmt, '-p [page #],', '--page=[page #]', 'specify result page number'));
+    }
 })();
 
-var kickass = require('../'),
+const kickass = require('../'),
     format = require('util').format,
     chalk = require('chalk'),
-    forEachRight = require('../lib/foreach-right'),
-    log = console.log,
+    forEachRight = require('lodash.foreachright'),
     PER_PAGE = 25,
     args = process.argv.slice(2),
-    page, category, search, field, sorder, magnet, color = require('supports-color');
+    color = require('supports-color');
 
-(function permuteArgv () {
-  var newArgs = [];
-  process.argv.forEach(function (v) {
-    if (/-[a-zA-Z]+$/.test(v)) {
-      newArgs = newArgs.concat(v.split('').slice(1).map(function (v) { return '-' + v; }));
-    } else newArgs.push(v);
-  });
-  process.argv = newArgs;
+let page, category, search, field, sorder, magnet, debug = false;
+
+(function permuteArgv() {
+    let newArgs = [];
+    process.argv.forEach(function(v) {
+        if (/-[a-zA-Z]+$/.test(v)) {
+            newArgs = newArgs.concat(v.split('').slice(1).map(function(v) {
+                return '-' + v;
+            }));
+        } else newArgs.push(v);
+    });
+    process.argv = newArgs;
 })();
 
-(function parseArgs () {
-  var catRegex = /--category=(.*$)/,
-      pageRegex = /--page=(\d+)/,
-      fieldRegex = /--field=(.*$)/,
-      sorderRegex = /--sorder=(.*$)/;
-  for (var i = 0; i < args.length; i++) {
-    if (args[i] === '-c') {
-      category = args.splice(i, 2)[1];
-      i--;
-    } else if (catRegex.test(args[i])) {
-      category = args.splice(i, 1)[0].match(catRegex)[1];
-      i--;
-    } else if (args[i] === '-p') {
-      page = +args.splice(i, 2)[1];
-      i--;
-    } else if (pageRegex.test(args[i])) {
-      page = +args.splice(i, 1)[0].match(pageRegex)[1];
-    } else if (args[i] === '-f') {
-      field = args.splice(i, 2)[1];
-      i--;
-    } else if (fieldRegex.test(args[i])) {
-      field = args.splice(i, 1)[0].match(fieldRegex)[1];
-    } else if (args[i] === '-s') {
-      sorder = args.splice(i, 2)[1];
-    } else if (sorderRegex.test(args[i])) {
-      sorder = args.splice(i, 1)[0].match(sorderRegex)[1];
-    } else if (args[i] === '--magnet' || args[i] === '-m') {
-      magnet = true;
-      args.splice(i, 1);
-    } else if (args[i] === '--no-color' || args[i] === '-n') {
-      color = false;
-      args.splice(i, 1);
+(function parseArgv() {
+    var catRegex = /--category=(.*$)/,
+        pageRegex = /--page=(\d+)/,
+        fieldRegex = /--field=(.*$)/,
+        sorderRegex = /--sorder=(.*$)/;
+    for (var i = 0; i < args.length; i++) {
+        if (args[i] === '-c') {
+            category = args.splice(i, 2)[1];
+            i--;
+        } else if (catRegex.test(args[i])) {
+            category = args.splice(i, 1)[0].match(catRegex)[1];
+            i--;
+        } else if (args[i] === '-p') {
+            page = +args.splice(i, 2)[1];
+            i--;
+        } else if (pageRegex.test(args[i])) {
+            page = +args.splice(i, 1)[0].match(pageRegex)[1];
+        } else if (args[i] === '-f') {
+            field = args.splice(i, 2)[1];
+            i--;
+        } else if (fieldRegex.test(args[i])) {
+            field = args.splice(i, 1)[0].match(fieldRegex)[1];
+        } else if (args[i] === '-s') {
+            sorder = args.splice(i, 2)[1];
+        } else if (sorderRegex.test(args[i])) {
+            sorder = args.splice(i, 1)[0].match(sorderRegex)[1];
+        } else if (args[i] === '--magnet' || args[i] === '-m') {
+            magnet = true;
+            args.splice(i, 1);
+        } else if (args[i] === '--no-color' || args[i] === '-n') {
+            color = false;
+            args.splice(i, 1);
+        } else if (args[i] === '--debug' || args[i] === '-d') {
+            debug = true;
+            args.splice(i, 1);
+        }
     }
-  }
-  search = args.join(' ');
+    search = args.join(' ');
 })();
 if (!page) page = 1;
 if (!color) neutralizeColor();
+if (debug) require('request-debug')(require('request'));
+
 function neutralizeColor() {
-  Object.keys(chalk.styles).forEach(function (v) {
-    chalk.styles[v].open = '';
-    chalk.styles[v].close = '';
-  });
+    Object.keys(chalk.styles).forEach(function(v) {
+        chalk.styles[v].open = '';
+        chalk.styles[v].close = '';
+    });
 }
 kickass({
-  search: search,
-  page: page,
-  category: category,
-  field: field,
-  sorder: sorder,
-  detailed: magnet
-}, function (err, results) {
-  if (err) return console.log(err);
-  var total = results.total_results;
-  forEachRight(results.list, function (v) {
-    log(format('%s%s - %s%s%s', chalk.styles.cyan.open, v.category, chalk.styles.bold.open + chalk.styles.yellow.open, v.title, chalk.styles.yellow.close + chalk.styles.bold.close));
-    log(format('%s%d %sSeeders %s/ %s%d %sLeechers%s', chalk.styles.bold.open + chalk.styles.green.open, v.seeds, chalk.styles.bold.close, chalk.styles.green.close, chalk.styles.bold.open + chalk.styles.red.open, v.leechs, chalk.styles.bold.close, chalk.styles.red.close));
-    log(magnet ? v.magnetLink : v.torrentLink);
-    log(format('%s%s%s', chalk.styles.magenta.open, v.pubDate, chalk.styles.magenta.close));
-    log();
-  });
-  log(format('%sDisplaying torrents %s%d %s- %s%d %sout of %s%d%s total.%s', chalk.styles.cyan.open, chalk.styles.yellow.open, PER_PAGE*(page - 1) + 1, chalk.styles.bold.close, chalk.styles.bold.open, Math.min(PER_PAGE*page, total), chalk.styles.bold.close + chalk.styles.cyan.open, chalk.styles.bold.open + chalk.styles.magenta.open, total, chalk.styles.bold.close + chalk.styles.cyan.open, chalk.styles.cyan.close));
+    search: search,
+    page: page,
+    category: category,
+    field: field,
+    sorder: sorder
+}).then(function(results) {
+    var total = results.total_results;
+    forEachRight(results.list, function(v, i) {
+        log(format('%s%s - %s%s%s', chalk.styles.cyan.open, v.category, (v.elite ? chalk.styles.magenta.open + chalk.styles.bold.open + 'ELITE ' + chalk.styles.magenta.close + chalk.styles.bold.close : '') + (v.verified ? chalk.styles.bold.open : '') + chalk.styles.yellow.open, v.title, chalk.styles.yellow.close + (v.verified ? chalk.styles.bold.close : '')));
+        log(format('%s%d %sSeeders %s/ %s%d %sLeechers%s', chalk.styles.bold.open + chalk.styles.green.open, v.seeds, chalk.styles.bold.close, chalk.styles.green.close, chalk.styles.bold.open + chalk.styles.red.open, v.leechs, chalk.styles.bold.close, chalk.styles.red.close));
+        log(magnet ? v.magnetLink : v.torrentLink);
+        log(format('%s%s%s', chalk.styles.magenta.open, v.pubDate, chalk.styles.magenta.close));
+        log();
+    });
+    log(format('%sDisplaying torrents %s%d %s- %s%d %sout of %s%d%s total.%s', chalk.styles.cyan.open, chalk.styles.yellow.open, PER_PAGE * (page - 1) + 1, chalk.styles.bold.close, chalk.styles.bold.open, Math.min(PER_PAGE * page, total), chalk.styles.bold.close + chalk.styles.cyan.open, chalk.styles.bold.open + chalk.styles.magenta.open, total, chalk.styles.bold.close + chalk.styles.cyan.open, chalk.styles.cyan.close));
+}, function(err) {
+    console.log(err.stack);
 });
--- test/test.js
+++ test/test.js
@@ -1,54 +1,149 @@
 #!/usr/local/bin/mocha
+
 var expect = require('chai').expect,
-    forEachRight = require('../lib/foreach-right'),
+    kickass = require('..'),
     dns = require('dns'),
-    err,
-    basename = require('../lib/basename');
+    results = require('../lib/results'),
+    cheerio = require('cheerio'),
+    selector = require('../lib/selector'),
+    KickassTorrentGroup = results.KickassTorrentGroup,
+    KickassTorrent = results.KickassTorrent,
+    URLS = require('../config/kickass-urls'),
+    uri = require('uri-js'),
+    fetch = require('../lib/fetch'),
+    merge = require('../lib/merge'),
+    searchPageResult,
+    err, katErr;
 
-  before(function (done) {
-dns.resolve('www.google.com', function (e) {
-  err = e;
-  done();
+before(function(done) {
+    Promise.all([new Promise(function(resolve, reject) {
+        dns.resolve('www.google.com', function(e) {
+            err = e;
+            resolve();
+        });
+    }), new Promise(function(resolve, reject) {
+        dns.resolve(uri.parse(URLS.url).host, function(e) {
+            katErr = e;
+            if (!katErr) kickass._requestRawBody('south park').then(function(response) {
+                searchPageResult = cheerio.load(response.body);
+                resolve();
+            }, function(err) {
+                resolve();
+            });
+        });
+    })]).then(function() {
+        done();
+    });
 });
-  });
 
-describe('forEach right module', function () {
-  it('should halt execution after returning false', function () {
-    var arr = [];
-    forEachRight([4, 3, 2, 1], function (v, i) {
-      this.push(v);
-      if (i === 2) return false;
-    }, arr);
-    expect(arr.length).to.equal(2);
-    expect(arr[0]).to.equal(1);
-    expect(arr[1]).to.equal(2);
-  });
-  it('should do forEach backwards', function () {
-    var arr = [];
-    forEachRight([4, 3, 2], function (v) {
-      this.push(v);
-    }, arr);
-    expect(arr.length).to.equal(3);
-    expect(arr[0]).to.equal(2);
-    expect(arr[1]).to.equal(3);
-    expect(arr[2]).to.equal(4);
-  });
+describe('uri merge module', function() {
+    it('should combine any number of URI segments', function() {
+        expect(merge('http://', 'www.google.com', 'path')).to.equal('http://www.google.com/path');
+    });
+    it('should combine a search query', function() {
+        expect(merge('http://kat.cr', 'usearch', encodeURIComponent('south park'))).to.equal('http://kat.cr/usearch/south%20park');
+    });
 });
-describe('basename module', function () {
-  it('should find the basename', function () {
-    expect(basename('/some/path/to/executable')).to.equal('executable');
-  });
-})
-describe('test runner network connectivity', (function () {
-  if (err) return function () {
-    it('test runner must be connected to the internet', function () {
-      expect(false).to.be.true;
-    });
-  };
-  return function () {
-    it('client is connected', function () {
-      expect(1).to.eql(1);
-    });
-  };
+
+describe('test runner network connectivity', (function() {
+    if (err) return function() {
+        it('test runner must be connected to the internet', function() {
+            expect(false).to.be.true;
+        });
+    };
+    return function() {
+        it('test runner is connected', function() {
+            expect(1).to.eql(1);
+        });
+    };
 })());
 
+if (!err) {
+    describe('status of ' + uri.parse(URLS.url).host, function() {
+        it('is still the domain name', function() {
+            expect(katErr).to.be.null;
+        });
+        if (!katErr) it('still serves json', function() {
+            fetch({
+                method: 'GET',
+                url: uri.resolve(URLS.url, URLS.jsonUrl),
+                qs: {
+                    q: 'south park'
+                }
+            }).then(function(resp) {
+                try {
+                    JSON.parse(resp.body);
+                    expect(true).to.be.true;
+                } catch (e) {
+                    expect(true).to.be.false;
+                }
+            }, function(err) {
+                expect(true).to.be.false;
+            });
+        });
+    });
+    if (!katErr) {
+        describe('web scraping selectors', function() {
+            it('should be able to select the results summary', function() {
+                expect(selector._selectResultSummary.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the even rows', function() {
+                expect(selector.selectEvenRows(searchPageResult).length).to.be.ok;
+            });
+            it('should be able to select the odd rows', function() {
+                expect(selector.selectOddRows(searchPageResult).length).to.be.ok;
+            });
+            it('should be able to select the magnet link', function() {
+                expect(selector._selectMagnetLink.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the torrent link', function() {
+                expect(selector._selectTorrentLink.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the main link and title', function() {
+                expect(selector._selectLinkAndTitle.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the creator', function() {
+                expect(selector._selectCreator.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the category', function() {
+                expect(selector._selectCategory.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the size', function() {
+                expect(selector._selectSize.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the seed count', function() {
+                expect(selector._selectSeeds.bind(null, searchPageResult)).to.not.throw;
+            });
+            it('should be able to select the leech count', function() {
+                expect(selector._selectLeechs.bind(null, searchPageResult)).to.not.throw;
+            });
+        });
+        describe('kickass module', function() {
+            it('returns results as a KickassTorrentGroup', function() {
+                kickass('south park').then(function(results) {
+                    expect(Object.getPrototypeOf(results)).to.equal(KickassTorrentGroup.prototype);
+                }, function(err) {
+                    expect(err).to.be.null;
+                });
+            });
+            it('can search by category', function() {
+                kickass({
+                    search: 'south park',
+                    category: 'tv'
+                }).then(function(results) {
+                    results.list.forEach(function(v) {
+                        expect(v.category).to.equal('tv');
+                    });
+                }, function(err) {
+                    expect(err).to.be.null;
+                });
+            });
+            it('can be passed a callback or return a promise', function() {
+                expect(kickass('south park')).to.be.an.instanceof(Promise);
+                expect(kickass('south park', function(err, results) {
+                    expect(err).to.be.null;
+                })).to.be.undefined;
+            });
+        });
+    }
+}
