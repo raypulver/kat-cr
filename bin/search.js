9f42910ab296f55a7c12a5e2b389830053060115
Hassan McKusick <Hassan.McKusick@gmail.com>
2016-06-09 17:56:13 -0400

    Quick cleanup

--- bin/search.js
+++ bin/search.js
@@ -68,14 +68,14 @@ const opts = [{
 const argParser = ArgvParser(opts);
 
 const help = UsageGenerator({
-  description: 'Usage: ' + process.title + ' [-mnvh] [-c CATEGORY] [-p PAGE] [-f FIELD] [-s SORTORDER] SEARCH',
-  opts: opts
+  description: `Usage: ${process.title} [-mnvh] [-c CATEGORY] [-p PAGE] [-f FIELD] [-s SORTORDER] SEARCH`,
+  opts
 });
 
 try {
   var args = argParser(process.argv);
 } catch (e) {
-  log(sprintf('%s: %s', process.title, e.message));
+  log(`${process.title}: ${e.message}`);
   help();
   exit(1);
 }
@@ -87,33 +87,36 @@ if (args.version) {
   log(require('../package').version);
   exit(0);
 }
-if (!args.page) args.page = 1;
+args = Object.assign({ page: 1 }, args);
 if (args['no-color'] || !color) util.neutralizeColor();
 if (args.debug) {
   log(args);
   require('request-debug')(require('request'));
 }
 if (!args.remaining.length) {
-  log(sprintf('%s: %s', process.title, 'Must supply a search query'));
+  log(`${process.title}: Must supply a search query`);
   help();
   exit(1);
 }
 kickass({
   search: args.remaining.join(' '),
-  page: args.page,
-  category: args.category,
-  field: args.field,
-  sorder: args.sorder
-}).then(function(results) {
-  var total = results.total_results;
+  page,
+  category,
+  field,
+  sorder
+} = args).then(function(results) {
+  let total = results.total_results,
+      { cyan, green, red, yellow, magenta, bold } = chalk.styles;
   forEachRight(results.list, function(v, i) {
-    log(format('%s%s - %s%s%s', chalk.styles.cyan.open, v.category, (v.elite ? chalk.styles.magenta.open + chalk.styles.bold.open + 'ELITE ' + chalk.styles.magenta.close + chalk.styles.bold.close : '') + (v.verified ? chalk.styles.bold.open : '') + chalk.styles.yellow.open, v.title, chalk.styles.yellow.close + (v.verified ? chalk.styles.bold.close : '')));
-    log(format('%s%d %sSeeders %s/ %s%d %sLeechers%s', chalk.styles.bold.open + chalk.styles.green.open, v.seeds, chalk.styles.bold.close, chalk.styles.green.close, chalk.styles.bold.open + chalk.styles.red.open, v.leechs, chalk.styles.bold.close, chalk.styles.red.close));
+    let elitePrefix = `${magenta.open}${bold.open}ELITE ${magenta.open}${bold.open}`;
+    let prefix = `${v.elite ? elitePrefix : ''}${v.verified ? bold.open : ''}${yellow.open}`,
+    log(`${cyan.open}${v.category}${cyan.close} - ${prefix}${v.title}${yellow.close}${v.verified ? bold.close : ''}`);
+    log(`${green.open}${bold.open}${v.seeds}${bold.close} Seeders${green.close} / ${red.open}${bold.open}${v.leechs}${bold.close} Leechers${red.close}`);
     log(args.magnet ? v.magnetLink : v.torrentLink);
-    log(format('%s%s%s', chalk.styles.magenta.open, v.pubDate, chalk.styles.magenta.close));
+    log(`${magenta.open}${v.pubDate}${magenta.close}`);
     log();
   });
-  log(format('%sDisplaying torrents %s%d %s- %s%d %sout of %s%d%s total.%s', chalk.styles.cyan.open, chalk.styles.yellow.open, PER_PAGE * (args.page - 1) + 1, chalk.styles.bold.close, chalk.styles.bold.open, Math.min(PER_PAGE * args.page, total), chalk.styles.bold.close + chalk.styles.cyan.open, chalk.styles.bold.open + chalk.styles.magenta.open, total, chalk.styles.bold.close + chalk.styles.cyan.open, chalk.styles.cyan.close));
+  log(`${yellow.open}${cyan.open}Displaying torrents${cyan.close} ${PER_PAGE * (args.page - 1) + 1} - ${bold.open}${Math.min(PER_PAGE * args.page, total)}${bold.close} ${cyan.open}out of${cyan.close} ${bold.open}${magenta.open}${total}{magenta.close}{bold.close} {cyan.open}total.${cyan.close}`);
 }, function(err) {
   console.log(err.stack);
 });
